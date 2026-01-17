f7872331e6a65737f2c783af9823e45912d6dc4e
RamizHHH <RamizHussain500@Gmail.com>
2026-01-16 22:57:46 -0700

    Worked on cpu_memory

---
+++ CPU_Memory/cpu_memory.c
@@ -0,0 +1,40 @@
+#include "cpu_memory.h"
+
+cpu_memory allocateMem()
+{
+    cpu_memory mainMemory;
+    mainMemory.memSize = 64 * 1024;
+    mainMemory.ptr = (uint32_t *)malloc(mainMemory.memSize);
+
+    if (mainMemory.memSize == 0)
+    {
+        printf("Failed to set memory size");
+    }
+    else if (mainMemory.ptr == NULL)
+    {
+        printf("Failed to allocate memory");
+    }
+
+    return mainMemory;
+}
+
+void deallocateMem(cpu_memory mainMemory)
+{
+    free(mainMemory.ptr);
+
+    if (mainMemory.ptr == NULL)
+    {
+        printf("deallocated memory!");
+    }
+}
+
+void mem_write(cpu_memory mainMemory, uint32_t address, uint32_t value)
+{
+    mainMemory.ptr[address] = value;
+}
+
+uint32_t mem_read(uint32_t address, cpu_memory mainMemory)
+{
+    uint32_t result = *(mainMemory.ptr + address);
+    return result;
+}
